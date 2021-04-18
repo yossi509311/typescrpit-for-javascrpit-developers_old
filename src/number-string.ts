6bbe23b09d6db6be4c6e685c9c630a2405fd51c7
yossi509311@gmail.com <yossi509311@gmail.com>
2021-04-18 12:33:29 +0900

    number-string レクチャー

---
+++ src/number-string.ts
@@ -0,0 +1,15 @@
+export {};
+
+let year: number = 1976;
+//year = false;
+
+console.log(year);
+
+let age: number = 0x2b;
+
+console.log(age);
+
+let name: string = 'Ham';
+//name = 1;
+console.log(name);
+
